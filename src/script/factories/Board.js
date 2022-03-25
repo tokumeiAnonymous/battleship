66ecf06b5133e0fd385bf17a26608330f4ee74cf
Jerome Tokumei <jerometokumei@gmail.com>
2022-03-25 21:40:59 +0800

    Change Board receiveAttack to use an array of status

--- src/script/factories/Board.js
+++ src/script/factories/Board.js
@@ -2,15 +2,31 @@ const CreateBoard = (ships) => {
   const shipPositions = [].concat(...(ships.map(ship => ship.getPositions())));
   let shipLife = 20;
 
+  // 1 = ship, 2 = neighbored a ship, 3 = empty -1,-2,-3 means received attack
+  const boardStatus = [];
+  // find ways for better implemetation using higher order function
+  for (let i = 0; i < 10; i++) {
+    boardStatus.push([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+  }
+
+  shipPositions.forEach( ([x, y]) => {
+    boardStatus[x][y] = 1;
+    // update neighbor here
+
+  });
+
   // has side effects
   const receiveAttack = (inPosition) => {
     const [ xIn, yIn] = inPosition;
+
+    // validate attack here
+    if (boardStatus[xIn][yIn] < 0) return false;
     
-    const isHit = shipPositions.find(element => {
-      const [x, y] = element;
-      return (x == xIn && y == yIn);
-    });
-    if (isHit) shipLife--;
+    else if (boardStatus[xIn][yIn] == 1) {
+      shipLife--;
+      boardStatus[xIn][yIn] = - (boardStatus[xIn][yIn]);
+    }
+    return true;
   }
 
   const getShipLifeCount = () => shipLife;
